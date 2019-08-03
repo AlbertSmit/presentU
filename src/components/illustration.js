97fa0a8b281c090273e7ac408fba006dc0316bb3
Albert <albert@diskus.studio>
2019-08-03 15:25:17 +0200

    add illustration

--- src/components/illustration.js
+++ src/components/illustration.js
@@ -3,10 +3,13 @@ import React from 'react'
 import './illustration.scss'
 
 export default function Illustration() {
+    const g = 'https://presentu.nl/images/Middel-43d.png';
+
     return (
         <div className='presentu-fn-comp-illustration'>
             <div className='giant-u-container' >
-                <p>U</p>
+                {/* <h1>U</h1> */}
+                <img src={g}></img>
             </div>
         </div>
     )
