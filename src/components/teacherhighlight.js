d85c949b4020fd6bf57a752cd86b4e3c845b2177
Albert <albert@diskus.studio>
2019-10-12 21:56:21 +0200

    Adapt images

--- src/components/teacherhighlight.js
+++ src/components/teacherhighlight.js
@@ -5,15 +5,16 @@ const textsus = "Sus Verbruggen is afgestudeerd aan de Mime opleiding op de Thea
 const textmaarten = "Maarten van den Berg is afgestudeerd aan de Koningstheateracademie in Den Bosch als cabaretier en behaalde zijn bachelor Humanistiek aan de Universiteit voor Humanistiek. Hij werkte mee aan Zondag met Lubach en aan Panache met Janjaap van der Wal en Tim Fransen. Hij is aangesloten bij Bos Theaterproducties en speelt met zijn eigen cabaretprogramma in theaters door het hele land. Maarten maakt maatschappelijk cabaret, ten koste van de politiek en zichzelf."
 
 const imgtemp = "https://presentu.nl/images/Middel-14d.png";
+const temp2 = 'https://presentu.nl/images/Middel-13d-p-800.png';
 
 export default function TeacherHighlight(props) {
     return (
         <div className='presentu-fn-comp-teacherhighlight'>
             <div className='content-width-rule'>
-                <div className='presentu-teacherhighlight-teachers'>
+                <div className='teachers'>
                     <div className='highlight' >
                         <div className='image'>
-                            <img src={imgtemp}/>
+                            <img src={temp2}/>
                         </div>
                         <div className='body'>
                             <h1>Sus Verbruggen</h1>
@@ -34,7 +35,7 @@ export default function TeacherHighlight(props) {
                             <img src={imgtemp}/>
                         </div>
                         <div className='body'>
-                            <h1>3</h1>
+                            <h1>Naam van Docent</h1>
                             <p>{textmaarten}</p>
                         </div>
                     </div>
@@ -43,7 +44,7 @@ export default function TeacherHighlight(props) {
                             <img src={imgtemp}/>
                         </div>
                         <div className='body'>
-                            <h1>4</h1>
+                            <h1>Naam van Docent</h1>
                             <p>{textmaarten}</p>
                         </div>
                     </div>
@@ -52,7 +53,7 @@ export default function TeacherHighlight(props) {
                             <img src={imgtemp}/>
                         </div>
                         <div className='body'>
-                            <h1>5</h1>
+                            <h1>Naam van Docent</h1>
                             <p>{textmaarten}</p>
                         </div>
                     </div>
