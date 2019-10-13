e403f826399ddbf866b4fea9d7ecafbba07a6455
Albert <albert@diskus.studio>
2019-10-13 20:23:28 +0200

    Move components for preview

--- src/pages/werkwijze.js
+++ src/pages/werkwijze.js
@@ -3,83 +3,13 @@ import { graphql, Link } from 'gatsby';
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import WhoWhatWhere from "../components/whowhatwhere";
+import Approach from "../components/approach";
 
 const WerkwijzePage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <WhoWhatWhere data={data}/>
+    <Approach />
   </Layout>
 )
-
-export const query = graphql`
-  query {
-    prismicFocus {
-    data {
-      broodtekst {
-        text
-      }
-      contact {
-        text
-      }
-      contact_content {
-        text
-        html
-      }
-      fysiek {
-        text
-      }
-      fysiek_content {
-        text
-        html
-      }
-      koptekst {
-        text
-      }
-      over {
-        text
-      }
-      over_content {
-        text
-        html
-      }
-      resultaat_content {
-        text
-        html
-      }
-      resultaatgericht {
-        text
-      }
-      stem {
-        text
-      }
-      stem_content {
-        text
-        html
-      }
-      waarom {
-        text
-      }
-      waarom_content {
-        text
-        html
-      }
-      wat {
-        text
-      }
-      wat_content {
-        text
-        html
-      }
-      wie {
-        text
-      }
-      wie_content {
-        text
-        html
-      }
-    }
-  }
-}`
 
 export default WerkwijzePage
