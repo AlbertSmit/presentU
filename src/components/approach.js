10a1a8eca3e07e8b4cb140edfd2c60f0be4de004
Albert <albert@diskus.studio>
2019-10-14 20:03:52 +0200

    Add additional bulletpoint to approach section

--- src/components/approach.js
+++ src/components/approach.js
@@ -12,13 +12,23 @@ export default function Approach(props) {
                         <h3 className='pre'>1</h3>
                         <div className='box'>
                             <h3 className='text-white'>Basisvaardigheden</h3>
-                            <hr/>
-                            <h5>Wat je leert</h5>
-                            <ul>
-                                <li>Plezier in presenteren</li>
-                                <li>Controle over zenuwen</li>
-                                <li>Technische vaardigheden</li>
-                                <li>Ontdek je stijl</li>
+                        </div>
+                        <div className='subbox'>
+                            <div className='spacer'>
+                                <h5>Wat je leert</h5>
+                                <ul>
+                                    <li>Plezier in presenteren</li>
+                                    <li>Controle over zenuwen</li>
+                                    <li>Technische vaardigheden</li>
+                                    <li>Ontdek je stijl</li>
+                                </ul>
+                            </div>
+                            <h5>Aan de hand van</h5>
+                            <ul className='what'>
+                                <li>0-meting</li>
+                                <li>Persoonlijk ontwikkelplan</li>
+                                <li>Basistraining presentatie</li>
+                                <li>Uitgebreide feedback van trainers en deelnemers</li>
                             </ul>
                         </div>
                     </div>
@@ -31,12 +41,21 @@ export default function Approach(props) {
                         <h3 className='pre'>2</h3>
                         <div className='box'>
                             <h3 className='text-white'>Story skills</h3>
-                            <hr/>
-                            <h5>Wat je leert</h5>
-                            <ul>
-                                <li>Je eigen verhaal vertellen</li>
-                                <li>Een verhaal opbouwen</li>
-                                <li>Schrijven voor verschillend publiek</li>
+                        </div>
+                        <div className='subbox'>
+                            <div className='spacer'>
+                                <h5>Wat je leert</h5>
+                                <ul>
+                                    <li>Je eigen verhaal vertellen</li>
+                                    <li>Een verhaal opbouwen</li>
+                                    <li>Schrijven voor verschillend publiek</li>
+                                </ul>
+                            </div>
+                            <h5>Aan de hand van</h5>
+                            <ul className='what'>
+                                <li>Story-telling training I & II</li>
+                                <li>Proces van schaven en verbeteren</li>
+                                <li>Leren wat werkt en niet werkt voor jou</li>
                             </ul>
                         </div>
                     </div>
@@ -49,13 +68,22 @@ export default function Approach(props) {
                         <h3 className='pre'>3</h3>
                         <div className='box'>
                             <h3 className='text-white'>Expertvaardigheden</h3>
-                            <hr/>
-                            <h5>Wat je leert</h5>
+                        </div>
+                        <div className='subbox'>
+                            <div className='spacer'>
+                                <h5>Wat je leert</h5>
+                                <ul className='what'>
+                                    <li>Oefenen voor groepen</li>
+                                    <li>Uitgebreide aandacht voor jouw focuspunten bij presenteren</li>
+                                    <li>Integreren van vaardigheden uit I & II</li>
+                                </ul>
+                            </div>
+                            <h5>Aan de hand van</h5>
                             <ul className='what'>
-                                <li>De goede keuzes maken in je presentatie</li>
-                                <li>Aanpassen aan verschillend publiek</li>
-                                <li>Overtuigen, impact maken</li>
-                                <li>Controle over stem, ruimte, lichaam</li>
+                                <li>0-meting</li>
+                                <li>Persoonlijk ontwikkelplan</li>
+                                <li>Basistraining presentatie</li>
+                                <li>Uitgebreide feedback van trainers en deelnemers</li>
                             </ul>
                         </div>
                     </div>
